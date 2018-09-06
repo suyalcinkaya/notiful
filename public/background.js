cc6b57ad19cb247f9fd45701e72b8ef224f0ca8b
Onur Suyalcinkaya <suyalcinkaya@live.com>
2018-09-07 00:39:54 +0300

    update

--- public/background.js
+++ public/background.js
@@ -1,1 +1,13 @@
-!function(e){function t(r){if(n[r])return n[r].exports;var i=n[r]={exports:{},id:r,loaded:!1};return e[r].call(i.exports,i,i.exports,t),i.loaded=!0,i.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}({0:function(e,t,n){try{(function(){"use strict";n(625)}).call(this)}finally{}},625:function(e,t,n){try{(function(){"use strict";chrome.runtime.onMessage.addListener(function(e,t){chrome.tabs.query({},function(n){n.map(function(n){return t.tab&&t.tab.id===n.id?!1:(chrome.tabs.sendMessage(n.id,e),!0)})})}),chrome.browserAction.onClicked.addListener(function(){chrome.tabs.query({url:"chrome://newtab/",title:"Notiful"},function(e){e.length<=0?chrome.tabs.create({url:chrome.extension.getURL("index.html")}):chrome.tabs.update(e[0].id,{selected:!0})})})}).call(this)}finally{}}});
+function open_editor_in_tab() {
+    chrome.tabs.create({ url: chrome.extension.getURL('index.html') });
+}
+
+/*if(!chrome.browserAction.onClicked.hasListener(open_editor_in_tab)) {
+    chrome.browserAction.onClicked.removeListener(open_editor_in_tab);
+}*/
+
+chrome.browserAction.onClicked.addListener(open_editor_in_tab);
+
+chrome.browserAction.setTitle({
+    title: 'Open Notiful'
+});
